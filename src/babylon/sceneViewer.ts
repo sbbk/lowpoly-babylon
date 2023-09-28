cccfc6bfd7b0c9a0f4bbe3cc68e0d1ae86bd4531
Elliot Turner <elliot.turner@visr-vr.com>
2023-09-28 16:16:03 +0100

    GUI Labels. Highlighting. Nukem Hands.

--- src/babylon/sceneViewer.ts
+++ src/babylon/sceneViewer.ts
@@ -3,8 +3,9 @@ import { jsx } from "@vertx/jsx";
 import { GLTFFileLoader } from "@babylonjs/loaders";
 import { GraphicsConfig, ModelLoader } from "../media/models/modelImporter";
 import { ShaderManager } from "./shaders/shaderManager";
+import * as GUI from "@babylonjs/gui"
 
-export class MainCamera extends BABYLON.ArcRotateCamera {
+export class MainCamera extends BABYLON.FreeCamera {
 
     defaultSpeed:number = 1;
     defaultFrameRate:number = 1;
@@ -65,6 +66,16 @@ export class InteractableModel {
     }
 }
 
+export interface PointOfInterest {
+
+    name:string,
+    target:BABYLON.Vector3,
+    alpha:number,
+    beta:number,
+    radius:number
+
+}
+
 export class SceneViewer {
 
     canvas:HTMLCanvasElement;
@@ -74,6 +85,8 @@ export class SceneViewer {
     mainLight:BABYLON.HemisphericLight;
     fileLoader:GLTFFileLoader;
     loadedModel:BABYLON.Scene
+    interestPoints:PointOfInterest[];
+    currentTarget:number;
 
     constructor(canvas:HTMLCanvasElement) {
         this.canvas = canvas;
@@ -82,7 +95,146 @@ export class SceneViewer {
         this.engine.setHardwareScalingLevel(2);
         this.scene = new BABYLON.Scene(this.engine);
         window["scene"] = this.scene;
-        this.camera = new MainCamera('main-camera',Math.PI / 2, Math.PI / -2, 10, BABYLON.Vector3.Zero());
+        this.camera = new MainCamera('main-camera',new BABYLON.Vector3(8.88988495700036,5.39056883665061,1.260390443856567));
+        this.camera.setTarget(new BABYLON.Vector3(7.892032691165552,5.355046364537239,1.205354059244245))
+        //this.camera.wheelDeltaPercentage = 0.1;
+
+        this.camera.checkCollisions = true;
+
+        //this.interestPoints = [new BABYLON.Vector3(1.751672367244813,1.94732104969539,0.06579143706881602),new BABYLON.Vector3(0.8125,1.125,-0.5),new BABYLON.Vector3(0,0,0.015625)];
+
+        this.interestPoints = [];
+        let interest1 = {
+
+            name:"1",
+            target:new BABYLON.Vector3(6.6991334075733375,5.844039807702413,0.526876081274739),
+            alpha:12.628530071921578,
+            beta:1.3971139740190135,
+            radius:0.0001,
+
+        } as PointOfInterest
+
+        let interest2 = {
+            name:"2",
+            target:new BABYLON.Vector3(4.076799374956906, 5.554503166394445,1.6728648703188291),
+            alpha:0.10409953845009177,
+            beta:0.8381139613149221,
+            radius:0.0001,
+        }
+
+        this.interestPoints.push(interest1);
+        this.interestPoints.push(interest2);
+
+        this.currentTarget = 0;
+        //this.camera.useFramingBehavior = true;
+
+        this.scene.onPointerObservable.add((pointerInfo, event) => {
+
+            switch(pointerInfo.type) {
+
+                case BABYLON.PointerEventTypes.POINTERTAP:
+                    if (pointerInfo.pickInfo && pointerInfo.pickInfo.pickedMesh) {
+
+                        console.log(pointerInfo.pickInfo.pickedMesh.position);
+
+                    }
+
+
+            }
+
+
+        })
+
+        let ease = (whichprop, targetval, speed) => {
+            var ease = new BABYLON.CubicEase();
+            ease.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+            BABYLON.Animation.CreateAndStartAnimation('at4', this.camera, whichprop, speed, 120, this.camera[whichprop], targetval, 0, ease);
+        }
+
+        document.addEventListener("keydown", (e) => {
+            if (e.key === 'a') {
+
+                let cameraTarget = this.interestPoints[this.currentTarget];
+                this.currentTarget = this.interestPoints.indexOf(cameraTarget);
+
+                e.preventDefault();
+                if (this.currentTarget == 0) {
+                    cameraTarget = this.interestPoints[this.interestPoints.length -1];
+                    this.currentTarget = this.interestPoints.indexOf(cameraTarget);
+                    //ease("position",cameraTarget.target,2000);
+
+                    ease("alpha",cameraTarget.alpha,2000);
+                    ease("beta",cameraTarget.beta,2000);
+                    ease("radius",cameraTarget.radius,2000);
+                    this.camera.target = cameraTarget.target;
+
+
+
+                }
+                else {
+                    console.log("ELSE",this.currentTarget);
+                    cameraTarget = this.interestPoints[this.currentTarget -1]
+                    console.log(cameraTarget);
+                    this.currentTarget = this.interestPoints.indexOf(cameraTarget);
+                    //ease("position",cameraTarget.target,2000);
+
+                    ease("alpha",cameraTarget.alpha,2000);
+                    ease("beta",cameraTarget.beta,2000);
+                    ease("radius",cameraTarget.radius,2000);
+                    this.camera.target = cameraTarget.target;
+
+                }
+            }
+            if (e.key === 'd') {
+
+                e.preventDefault();
+                let cameraTarget = this.interestPoints[this.currentTarget];
+                console.log(cameraTarget)
+
+                if (this.currentTarget == this.interestPoints.length -1) {
+                    cameraTarget = this.interestPoints[0];
+                    this.currentTarget = this.interestPoints.indexOf(cameraTarget);
+                    //ease("position",cameraTarget.target,2000);
+
+                    ease("alpha",cameraTarget.alpha,2000);
+                    ease("beta",cameraTarget.beta,2000);
+                    ease("radius",cameraTarget.radius,2000);
+                    this.camera.target = cameraTarget.target;
+
+
+
+                }
+                else {
+                    cameraTarget = this.interestPoints[this.currentTarget +1];
+                    this.currentTarget = this.interestPoints.indexOf(cameraTarget);
+                    
+                    //ease("position",cameraTarget.target,2000);
+                    ease("alpha",cameraTarget.alpha,2000);
+                    ease("beta",cameraTarget.beta,2000);
+                    ease("radius",cameraTarget.radius,2000);
+                    this.camera.target = cameraTarget.target;
+
+                }
+            }
+        });
+
+        let dukeHands = document.getElementById('duke-hands')
+        document.addEventListener('click',() => {
+            setTimeout(() => {
+                dukeHands.style.transform = "translateX(-1500px)"
+            }, 100);
+            setTimeout(() => {
+                dukeHands.style.transform = "translateX(-3000px)"
+            }, 200);
+            setTimeout(() => {
+                dukeHands.style.transform = "translateX(-4500px)"
+            }, 300);
+            setTimeout(() => {
+                dukeHands.style.transform = "translateX(0px)"
+            }, 400);
+        })
+
+
         window["camera"] = this.camera;
         this.mainLight = new BABYLON.HemisphericLight('main-light',BABYLON.Vector3.Zero());
         let graphics = new GraphicsConfig();
@@ -90,9 +242,11 @@ export class SceneViewer {
         // HARDWARE SCALING
         let hardwareScalingInput = document.getElementById("hardware-scaling-range") as HTMLInputElement;
         hardwareScalingInput.value = GraphicsConfig.hardwareScaling.toString();
+        console.log(GraphicsConfig.hardwareScaling);
         hardwareScalingInput.addEventListener('input',() =>{
             GraphicsConfig.setValue("hardwareScaling",parseFloat(hardwareScalingInput.value));
             this.engine.setHardwareScalingLevel(GraphicsConfig.hardwareScaling);
+            console.log(GraphicsConfig.hardwareScaling);
         })
 
         // JITTER
@@ -132,15 +286,21 @@ export class SceneViewer {
         this.camera.attachControl()
 
 
-        this.scene.registerBeforeRender(() => {
-            // Get the camera forward vector
-        });
+
             
         this.engine.runRenderLoop(() => {
             this.scene.render();
         });
     
         this.scene.onBeforeRenderObservable.add(() => {
+
+
+
+        })
+
+        document.addEventListener("fewfew",(e:CustomEvent) => {
+
+            
 
         })
 
@@ -159,10 +319,12 @@ export class SceneViewer {
                     if (currentIndex == 0) {
                         cameraTarget = loadedModels[loadedModels.length -1];
                         this.camera.setTarget(cameraTarget);
+
                     }
                     else {
                         cameraTarget = loadedModels[currentIndex +1]
                         this.camera.setTarget(cameraTarget);
+                        this.camera.radius = 3;
                     }
                 }
                 if (e.key === 'd') {
@@ -176,6 +338,7 @@ export class SceneViewer {
                     else {
                         cameraTarget = loadedModels[currentIndex +1]
                         this.camera.setTarget(cameraTarget);
+                        this.camera.radius = 3;
                     }
                 }
             });
@@ -201,9 +364,109 @@ export class SceneViewer {
         })
 
         // LOAD ALL MODELS AND DISTRUBUTE
-        ModelLoader.LoadAllModels(this.scene);
+        //ModelLoader.LoadAllModels(this.scene);
+        this.loadedModel = ModelLoader.LoadModel("Scene",this.scene,true);
+        let rayHelper :BABYLON.RayHelper;
+
+        let highlightLayer = new BABYLON.HighlightLayer('hl-l',this.scene);
+        let highlightedMeshes:BABYLON.Mesh[] = [];
+        var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        var rect1 = new GUI.Rectangle();
+        rect1.width = 0.3;
+        rect1.height = "100px";
+        rect1.cornerRadius = 20;
+        rect1.color = "Orange";
+        rect1.thickness = 4;
+        rect1.background = "green";
+        advancedTexture.addControl(rect1);
+        rect1.linkOffsetY = -150;
     
-        
+        var label = new GUI.TextBlock();
+        label.text = "Sphere";
+        label.fontSize = "100px"
+        rect1.addControl(label);
+    
+        var tag = new GUI.Ellipse();
+        tag.width = "40px";
+        tag.height = "40px";
+        tag.color = "Orange";
+        tag.thickness = 4;
+        tag.background = "green";
+        advancedTexture.addControl(tag);
+ 
+  
+    
+        var line =  new GUI.Line();
+        line.lineWidth = 4;
+        line.color = "Orange";
+        line.y2 = 20;
+        line.linkOffsetY = -20;
+        advancedTexture.addControl(line);
+
+        line.connectedControl = rect1;  
+
+
+        this.scene.registerBeforeRender(() => {
+            setTimeout(() => {                
+                if (rayHelper) {
+                    rayHelper.dispose();
+                }
+            }, 10);
+            let target = this.camera.getTarget();
+            let ray = BABYLON.Ray.CreateNewFromTo(this.camera.position,target);
+            ray.length = 10000;
+
+            let hit = this.scene.pickWithRay(ray);
+            highlightLayer.removeAllMeshes();
+            if (hit.pickedMesh) {
+                
+                highlightLayer.isEnabled = true;
+                let mesh = hit.pickedMesh as BABYLON.Mesh;
+                label.text = mesh.name;
+                line.linkWithMesh(mesh);
+                tag.linkWithMesh(mesh); 
+                rect1.linkWithMesh(mesh);   
+                // Lets try spawning random text thing.
+                //let textPlane = BABYLON.MeshBuilder.CreatePlane('text-plane',{size:2},this.scene);
+                // let cube = BABYLON.MeshBuilder.CreateBox('box');
+                // cube.isPickable = false;
+  
+
+                // textPlane.material = new BABYLON.StandardMaterial('mat');
+                // let textPlaneMat = new BABYLON.BackgroundMaterial('text-mat');
+                // textPlane.material = textPlaneMat;
+                // let planeTex = new BABYLON.DynamicTexture('text-tex',this.scene);
+                // textPlaneMat.diffuseTexture = planeTex;
+                // textPlaneMat.diffuseTexture.hasAlpha = true;
+                // textPlaneMat.alphaMode = 6;
+
+                // let ctx = planeTex.getContext();
+                // ctx.font = '20px Lato';
+                // let canvas = document.createElement("canvas");
+                // let context = canvas.getContext("2d");
+                // context.font = ctx.font;
+                // let text = mesh.name;
+                // planeTex.drawText(text,0,0,ctx.font,'white',"transparent",true,true)
+
+
+                let parent = mesh.parent;
+                if (!parent) return;
+                let meshes = parent.getChildMeshes();
+
+                meshes.forEach(mesh => {
+                    highlightLayer.addMesh(mesh, new BABYLON.Color3(1,1,0));
+                })
+            }
+            if (!hit.pickedMesh) {
+                
+                if (highlightedMeshes.length > 0) {
+                    highlightedMeshes.forEach(mesh => {
+                        highlightLayer.removeMesh(mesh);
+                    })
+                }
+                highlightLayer.isEnabled = false;
+            } 
+        });
 
         BABYLON.Effect.ShadersStore["customFragmentShader"] = `
         #ifdef GL_ES
@@ -234,7 +497,7 @@ export class SceneViewer {
             effect.setFloat2("screenSize", postProcess.width, postProcess.height);
             effect.setFloat("threshold", 1.0);
         };        
-        //let colorTable = new URL('./lut-posterized.png',import.meta.url).pathname;
+        let colorTable = new URL('./lut-posterized.png',import.meta.url).pathname;
         //let colorCorrectionProcess = new BABYLON.ColorCorrectionPostProcess("color-correction",colorTable,1.0,this.camera);
 
 
--- src/media/models/modelImporter.ts
+++ src/media/models/modelImporter.ts
@@ -15,7 +15,7 @@ export class GraphicsConfig {
         GraphicsConfig.ditherDarkenFactor = 1.2;
         GraphicsConfig.ditherLightenFactor = 0.1;
         GraphicsConfig.jitterAmplitude = 0.0010;
-        GraphicsConfig.hardwareScaling = 0.8;
+        GraphicsConfig.hardwareScaling = 1;
 
     }
 
@@ -51,6 +51,7 @@ export namespace ModelLoader {
     const trestle = new URL ('./trestle/mesh.gltf',import.meta.url).pathname;
     const cityScene = new URL ('./city_scene.glb',import.meta.url).pathname;
     export var LoadedModels:BABYLON.AbstractMesh[] = [];
+    export var LoadedModel:BABYLON.AbstractMesh;
 
     export type models = "Scene" | "CityScene"| "CrashBandicoot" | "dva" | "CheesePlant" | "MetalCabinet" | "Maschine" | "Monitor" | "TrestleTable";
 
@@ -100,13 +101,152 @@ export namespace ModelLoader {
             let loadedScene;
             BABYLON.SceneLoader.LoadAssetContainer(path, "", scene, ((container) => {
                 container.addAllToScene();
-                container.meshes[0].scalingDeterminant = 2;
+                let meshes = container.meshes;
+                meshes[0].scalingDeterminant = 2;
+                meshes[0].checkCollisions = true;
                 LoadedModels.push(container.meshes[0]);
 
                 const myEvent = new CustomEvent("modelLoader:Loaded", { detail: { mesh: container.meshes[0] } })
                 document.dispatchEvent(myEvent);
 
-                container.meshes[0].normalizeToUnitCube();
+                meshes.forEach(mesh => {
+                    if (!mesh.material) return;
+                    if (mesh.material.albedoTexture) {
+    
+                        BABYLON.Effect.ShadersStore["customVertexShader"]= "\r\n"+   
+                        "precision highp float;\r\n"+
+                
+                        "// Attributes\r\n"+
+                        "attribute vec3 position;\r\n"+
+                        "attribute vec2 uv;\r\n"+
+                
+                        "// Uniforms\r\n"+
+                        "uniform mat4 worldViewProjection;\r\n"+
+                        "uniform vec3 cameraForward;\r\n"+
+                        "uniform float noiseAmp;\r\n"+
+                        "uniform float alphaCutoff;\r\n"+            
+                
+                        "// Varying\r\n"+
+                        "varying vec2 vUV;\r\n"+
+                
+                        "void main(void) {\r\n"+
+                        "    vec3 noise = vec3(0.0, 0.0, 0.0);\r\n"+
+                        "    float noiseAmp = dot(cameraForward, position) * noiseAmp;\r\n"+
+                        "    noise.x = sin(position.x + uv.y * 10.0) * noiseAmp;\r\n"+
+                        "    noise.y = cos(position.y + uv.x * 10.0) * noiseAmp;\r\n"+
+                        "    noise.z = sin(position.z + uv.x * uv.y * 10.0) * noiseAmp;\r\n"+
+                        "    vec3 newPosition = position + noise;\r\n"+
+                        "    gl_Position = worldViewProjection * vec4(newPosition, 1.0);\r\n"+
+                
+                        "    vUV = uv;\r\n"+
+                        "}\r\n";
+                
+                    BABYLON.Effect.ShadersStore["customFragmentShader"]="\r\n"+
+                       "precision highp float;\r\n"+
+                
+                        "varying vec2 vUV;\r\n"+
+                
+                        "uniform sampler2D textureSampler;\r\n"+
+                        "uniform float noiseAmp;\r\n"+
+                        "uniform float ditherLightenFactor;\r\n"+
+                        "uniform float ditherDarkenFactor;\r\n"+
+                        "uniform float alphaCutoff;\r\n"+
+                
+                        "// Dithering pattern\r\n"+
+                        "const mat4 dither = mat4(\r\n"+
+                        "    0.0,  8.0,  2.0, 10.0,\r\n"+
+                        "    12.0,  4.0, 14.0,  6.0,\r\n"+
+                        "    3.0, 11.0,  1.0,  9.0,\r\n"+
+                        "    15.0,  7.0, 13.0,  5.0\r\n"+
+                        ");\r\n"+
+    
+                        "void main(void) {\r\n"+
+                        "   vec4 color = texture2D(textureSampler, vUV);\r\n"+
+                        "   float luminance = dot(color.rgb, vec3(0.2126, 0.7152, 0.0722));\r\n"+
+                        "   float threshold = dither[int(mod(gl_FragCoord.x, 4.0)), int(mod(gl_FragCoord.y, 4.0))].x / 16.0;\r\n"+
+                        "    if (luminance < threshold) {\r\n"+
+                    "        // Darken the color by multiplying by a factor\r\n"+
+                    "        color.rgb *= ditherDarkenFactor;\r\n"+
+                    "    } else {\r\n"+
+                    "        // Brighten the color by adding a constant\r\n"+
+                    "        color.rgb += vec3(ditherLightenFactor);\r\n"+
+                    "    }\r\n"+
+    
+                        
+                        "   float alpha = color.a;\r\n"+
+                        "   if (alpha < alphaCutoff)\r\n"+
+                        "    {\r\n"+
+                        "   discard;\r\n"+
+                        "    }\r\n"+
+                        "    gl_FragColor = vec4(color.r, color.g, color.b, color.a);\r\n"+
+                        "}\r\n";
+                
+                
+                    let alphaCutoff = 0.5;
+                    var shaderMaterial = new BABYLON.ShaderMaterial("shader", scene, {
+                        vertex: "custom",
+                        fragment: "custom",
+                        },
+                        {
+                            attributes: ["position", "normal", "uv"],
+                            uniforms: ["world", "worldView", "worldViewProjection", "view", "projection", "cameraForward", "noiseAmp","alphaCutoff","ditherLightenFactor","ditherDarkenFactor"],
+                            
+    
+                        });
+                
+    
+                
+                    shaderMaterial.setTexture("textureSampler", mesh.material.albedoTexture);
+                    shaderMaterial.setFloat("alphaCutoff",alphaCutoff)
+                    shaderMaterial.backFaceCulling = false;
+                
+                    mesh.material = shaderMaterial;
+                    window["reset"] = () => {
+                        shaderMaterial.forceCompilation(mesh);
+                    }
+    
+                    scene.registerBeforeRender(function () {
+                        // Get the camera forward vector
+            
+                    // var currentPosition = scene.activeCamera!.position.clone();
+            
+                    // // Get the distance between the previous and current position
+                    // var distance = BABYLON.Vector3.Distance(previousPosition, currentPosition);
+            
+                    // // Get the time elapsed since the last frame in seconds
+                    // var deltaTime = scene.getEngine().getDeltaTime() / 1000;
+            
+                    // // Calculate the speed as distance divided by time
+                    // speed = distance / deltaTime;
+            
+                    // Update the previous position with the current position
+                    //previousPosition.copyFrom(currentPosition);
+            
+                    var lighten = GraphicsConfig.ditherLightenFactor;
+                    var darken = GraphicsConfig.ditherDarkenFactor;
+                    var noiseAmp = Math.random() * GraphicsConfig.jitterAmplitude;
+            
+                    // var threshold = 0.05
+                    // if (speed < threshold) {
+                    //     noiseAmp = 0.0
+                    // }
+                    var cameraForward = scene.activeCamera!.getForwardRay().direction;
+                    // Pass it to the shader material as a uniform variable
+                    shaderMaterial.setVector3("cameraForward", cameraForward);
+                    shaderMaterial.setFloat("noiseAmp", noiseAmp);
+                    shaderMaterial.setFloat("ditherDarkenFactor",darken);
+                    shaderMaterial.setFloat("ditherLightenFactor",lighten);
+            
+                });
+    
+                        // let shader = new ShaderManager.VertexNoiseShader(scene);
+                        // shader.shaderMaterial.setTexture("shader-tex",mesh.material.albedoTexture);
+                        // shader.shaderMaterial.backFaceCulling = false;
+                        // mesh.material = shader.shaderMaterial;
+                    }
+                })
+
+                meshes[0].normalizeToUnitCube();
                 let rootNodes = container.rootNodes
                 for (var mNode of rootNodes) {
                     var posVector = (new BABYLON.Vector3(radius * Math.sin(startingVal*2*Math.PI/models.length),0, radius * Math.cos(startingVal*2*Math.PI/models.length)));
@@ -154,14 +294,18 @@ export namespace ModelLoader {
             }
         }
         let path = importModel(model);
-        let loadedScene;
+        let loadedScene : BABYLON.Scene;
         BABYLON.SceneLoader.Append(path, "", scene, ((node) => {
             loadedScene = node as BABYLON.Scene;
             let meshes = loadedScene.meshes;
+            console.log("Loaded Scene",loadedScene);
+            console.log("Meshes",meshes);
             ModelLoader.LoadedModel = meshes[0];
+            meshes[0].scaling = new BABYLON.Vector3(3,3,-3);
             //meshes[0].normalizeToUnitCube();
             
             meshes.forEach(mesh => {
+
                 if (!mesh.material) return;
                 if (mesh.material.albedoTexture) {
 
